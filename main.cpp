21a81c3592db7ce4256bb2b79e6d117f60a5bc84
tigrig29 <tigrig.crex@gmail.com>
2016-10-18 14:06:26 +0900

    codeadd

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	double a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 8, h = 2, j = 2;
+
+	printf("plus=%f\n"
+		"minus%f\n"
+		"multi=%f\n"
+		"division=%f\n"
+		"power=%f\n", plus(a, b), minus(c, d), multi(e, f), division(g, h), power(j));
+	return 0;
+}
