028819bd5c3ff17524e06d396c1e9bd7f1a62ce1
tigrig29 <tigrig.crex@gmail.com>
2016-10-18 15:46:21 +0900

    confliPlease

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ double plus(double a, double b){
 	return a + b;
 }
 
+double minusu(double c, double d){
+	return c - d;
+}
+
 int main(void){
 	double a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 8, h = 2, j = 2;
 
